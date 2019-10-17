1529afb5acaa3f5d1494d949655de9892f09ad13
Rafael Tongson <rtongson@peppymiint.local>
2019-10-17 14:57:43 -0700

    finish setting up routes and views

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace PortfolioProject
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseMvc();
         }
                 public Startup(IHostingEnvironment env)
