1d2a326a9479faea01fc17dae484c1f74620af8f
Rafael Tongson <rtongson@peppymiint.local>
2019-10-17 13:46:12 -0700

    set up basic routes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,10 +4,28 @@ namespace PortfolioProject
 {
 public class HomeController : Controller
   {
+    // START CONTROLLER //
+   //////////////////////
     [HttpGet("")]
     public string Index()
     {
-      return "Hello from index";
+      return "This is my Index!";
     } 
+
+    [HttpGet("projects")]
+    public string Projects()
+    {
+      return "This are my Projects!";
+    } 
+
+    [HttpGet("Contact")]
+    public string Contact()
+    {
+      return "This is my Contact!";
+    } 
+
+
+     ////////////////////
+    // END CONTROLLER //
   }
 }
